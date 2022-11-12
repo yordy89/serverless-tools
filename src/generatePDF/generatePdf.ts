b7bc846337e2a76c1c8a026da506672eabf9e918
yordyat9 <yalbelo@kreitech.com.uy>
2022-11-12 15:08:39 -0500

    chore: update all files

---
+++ src/generatePDF/generatePdf.ts
@@ -0,0 +1,35 @@
+// @ts-nocheck
+import pdf from 'pdf';
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { upload } from '../microservice/document.microservice';
+
+const handler = async (event: APIGatewayProxyEvent) => {
+  const { html, name, prefix } = JSON.parse(event.body);
+  try {
+    if (!html) {
+      throw new Error('No html provided');
+    }
+    const file = await pdf.generate(html);
+
+    const uploadDocResponse = await upload(
+      event.headers.Authorization,
+      name,
+      file,
+      prefix
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(uploadDocResponse)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: error.message
+      })
+    };
+  }
+};
+
+export { handler };
---
+++ src/generatePDF/generatePdfSystem.ts
@@ -0,0 +1,42 @@
+// @ts-nocheck
+import pdf from 'pdf';
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { upload } from '../microservice/document.microservice';
+
+const handler = async (event: APIGatewayProxyEvent) => {
+  const { html, name, prefix, companyId, userId } = JSON.parse(event.body);
+  try {
+    if (!html) {
+      throw new Error('No html provided');
+    }
+
+    const file = await pdf.generate(html);
+    const user = {
+      companyId,
+      user: {
+        id: userId
+      }
+    };
+    const uploadDocResponse = await upload(
+      event.headers.Authorization,
+      name,
+      file,
+      prefix,
+      user
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(uploadDocResponse)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: error.message
+      })
+    };
+  }
+};
+
+export { handler };
